9583bbf8d2157183cc1407db56a0aa114a37bac3
yshima <shimazuyuki7@gmail.com>
2025-04-11 18:15:56 +0900

    chore: マークダウンなども英語対応

--- apps/lp/src/app/[locale]/contact/page.tsx
+++ apps/lp/src/app/[locale]/contact/page.tsx
@@ -1,8 +1,17 @@
 import fs from "fs";
 import path from "path";
 
+import { useTranslations } from "next-intl";
+
 import { DocumentLayout } from "@/components/layout/document-layout";
 import { MarkdownContent } from "@/components/sections/markdown-content";
+
+import { routing } from "@/i18n/routing";
+
+// SSG対応
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 
 export function generateMetadata() {
   return {
@@ -12,17 +21,21 @@ export function generateMetadata() {
 }
 
 export default function ContactPage() {
+  const t = useTranslations("contact");
+  const config = useTranslations("config");
+
   // ビルド時にファイルを読み込む
   const filePath = path.join(
     process.cwd(),
     "public",
     "documents",
+    config("language-code"),
     "contact.md",
   );
   const fileContent = fs.readFileSync(filePath, "utf8");
 
   return (
-    <DocumentLayout title="お問い合わせ">
+    <DocumentLayout title={t("title")}>
       <MarkdownContent content={fileContent} />
     </DocumentLayout>
   );
