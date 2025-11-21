7190dba9510a2101b7305c0fa8b6036f56e8bf78
hyoshie <dymn9.term@gmail.com>
2025-11-21 17:02:43 +0900

    chore: リントエラー

--- apps/with-supabase/eslint.config.mjs
+++ apps/with-supabase/eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["*.ts", "*.tsx"], // 読み込むファイル
+  },
+  {
+    ignores: ["**/.next/**/*", "*.config.mjs", ".pnp.*"], // 無視するファイル
+  },
 ];
 
 export default eslintConfig;
